1923279768693514cfa5056935c2996f7ae49085
FrostyCoffee <taradapabintang@gmail.com>
2026-03-16 20:42:15 +0700

    feat: added Tiptap's text editor template

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Tiptap from "../Tiptap";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -7,7 +8,9 @@ export const Route = createFileRoute("/")({
 function Home() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3 className="mb-2">Welcome Home!</h3>
+      <Tiptap />
     </div>
   );
 }
+
--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [tailwindcss(), tanstackRouter({ target: "react", autoCodeSplitting: true }), react()],
 });
